3532a2b6a8ee92cb47e2ec897b75a0799c926ba5
Lybrook <omoshlybrook@gmail.com>
2024-11-15 07:15:03 +0300

    Changes done to PetList

--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import PetCard from './PetCard';
 
-const PetList = ({ pets, onFavorite }) => {
+const PetList = ({ pets, onFavorite, onDelete, onUpdate, }) => {
   return (
     <div className="pet-list">
       {pets.map(pet => (
-        <PetCard key={pet.id} pet={pet} onFavorite={onFavorite} />
+        <div key={pet.id}>
+          <PetCard 
+            pet={pet} 
+            onFavorite={onFavorite} 
+            onUpdate={onUpdate} 
+            onDelete={onDelete} 
+          />
+        </div>
       ))}
     </div>
   );
